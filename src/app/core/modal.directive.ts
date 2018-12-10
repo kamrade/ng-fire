e42745f6fef004c56e32124a42968be9e8db6d0e
Denis Michailov <kamrad.reg@gmail.com>
2018-12-10 22:37:55 +1000

    arrange code

--- src/app/core/modal.directive.ts
+++ src/app/core/modal.directive.ts
@@ -16,43 +16,43 @@ export class ModalComponent implements OnInit, OnDestroy {
     }
 
     ngOnInit(): void {
-        let modal = this;
+      let modal = this;
 
-        // ensure id attribute exists
-        if (!this.id) {
-            console.error('modal must have an id');
-            return;
+      // ensure id attribute exists
+      if (!this.id) {
+        console.error('modal must have an id');
+        return;
+      }
+
+      // move element to bottom of page (just before </body>) so it can be displayed above everything else
+      document.body.appendChild(this.element);
+
+      // close modal on background click
+      this.element.addEventListener('click', function (e: any) {
+        if (e.target.className === 'modal') {
+          modal.close();
         }
+      });
 
-        // move element to bottom of page (just before </body>) so it can be displayed above everything else
-        document.body.appendChild(this.element);
-
-        // close modal on background click
-        this.element.addEventListener('click', function (e: any) {
-            if (e.target.className === 'modal') {
-                modal.close();
-            }
-        });
-
-        // add self (this modal instance) to the modal service so it's accessible from controllers
-        this.modalService.add(this);
+      // add self (this modal instance) to the modal service so it's accessible from controllers
+      this.modalService.add(this);
     }
 
     // remove self from modal service when directive is destroyed
     ngOnDestroy(): void {
-        this.modalService.remove(this.id);
-        this.element.remove();
+      this.modalService.remove(this.id);
+      this.element.remove();
     }
 
     // open modal
     open(): void {
-        this.element.style.display = 'block';
-        document.body.classList.add('modal-open');
+      this.element.style.display = 'block';
+      document.body.classList.add('modal-open');
     }
 
     // close modal
     close(): void {
-        this.element.style.display = 'none';
-        document.body.classList.remove('modal-open');
+      this.element.style.display = 'none';
+      document.body.classList.remove('modal-open');
     }
 }
